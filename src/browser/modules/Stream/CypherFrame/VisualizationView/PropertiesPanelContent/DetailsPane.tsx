96eb6c2c629e1a3cd408314d06da588ce556a2e9
Jack <jack20220723@gmail.com>
2023-10-25 17:12:46 +0800

    Make node label and properties editable inspector panel (#15)

--- src/browser/modules/Stream/CypherFrame/VisualizationView/PropertiesPanelContent/DetailsPane.tsx
+++ src/browser/modules/Stream/CypherFrame/VisualizationView/PropertiesPanelContent/DetailsPane.tsx
@@ -30,7 +30,8 @@ export const DETAILS_PANE_STEP_SIZE = 1000
 export function DetailsPane({
   vizItem,
   graphStyle,
-  nodeInspectorWidth
+  nodeInspectorWidth,
+  onGraphInteraction
 }: DetailsPaneProps): JSX.Element {
   const [maxPropertiesCount, setMaxPropertiesCount] = useState(
     DETAILS_PANE_STEP_SIZE
@@ -82,6 +83,8 @@ export function DetailsPane({
                   label,
                   propertyKeys: vizItem.item.propertyList.map(p => p.key)
                 }}
+                onGraphInteraction={onGraphInteraction}
+                nodeId={vizItem.item.id}
               />
             )
           })}
@@ -93,6 +96,7 @@ export function DetailsPane({
           moreStep={DETAILS_PANE_STEP_SIZE}
           totalNumItems={allItemProperties.length}
           nodeInspectorWidth={nodeInspectorWidth}
+          onGraphInteraction={onGraphInteraction}
         />
       </PaneBody>
     </PaneWrapper>
